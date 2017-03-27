d1d130c236c650d6b9dd33e2d581b15c8e5283a1
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-03-27 13:36:49 +0200

    Settings ste for Hosting on Stud.hosted.hr.nl

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$dbpass = "";
-$dbname = "groentego";
+$username = "0925251";
+$dbpass = "xeinahzu";
+$dbname = "0925251";
 
 $err_level = error_reporting(0);
 $db = mysqli_connect($servername, $username, $dbpass, $dbname);
